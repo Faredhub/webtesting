22a60db57a31a9eb498c8ffd579b465da3714894
Faredhub <onlinekracker03@gmail.com>
2025-02-09 16:13:28 +0530

    Updated App.jsx with new functionality

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,31 @@ function App() {
         
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-            <button className="outline-none px-4 
+            <button 
+            onClick={() => setColor("red")}
+            className="outline-none px-4 
             py-1 rounded-full text-white shadow-lg"
             style={{backgroundColor: "red"}}>Red</button>
-            <button className="outline-none px-4 
+            <button
+             onClick={() => setColor("green")} 
+            className="outline-none px-4 
             py-1 rounded-full text-white shadow-lg"
             style={{backgroundColor: "green"}}>Green</button>
-            <button className="outline-none px-4 
+            <button 
+             onClick={() => setColor("violet")}
+             className="outline-none px-4 
             py-1 rounded-full text-white shadow-lg"
             style={{backgroundColor: "violet"}}>Violet</button>
+            <button 
+             onClick={() => setColor("black")}
+             className="outline-none px-4 
+            py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor: "black"}}>Black</button>
+            <button 
+             onClick={() => setColor("nevy")}
+             className="outline-none px-4 
+            py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor: "Nevy"}}>Nevy</button>
           </div>
         </div>
 
